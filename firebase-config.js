f9dd225b3a604e0fb38bf9b242270a920fc04353
Osaid2993 <osaidbahadry@gmail.com>
2026-09-04 16:58:05 +1000

    Add real-time claims via Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: "AIzaSyDXIy9xKeIpy_Aa2i_xHHibd2CKB_Q-yCQ",
   authDomain: "splashkit-tracker.firebaseapp.com",
+  databaseURL: "https://splashkit-tracker-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "splashkit-tracker",
   storageBucket: "splashkit-tracker.firebasestorage.app",
   messagingSenderId: "719014220435",
